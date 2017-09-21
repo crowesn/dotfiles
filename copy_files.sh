3cb6220a9df61b57fbacdb1e138a7e7f18b5bfa5
crowesn <crowesn@gmail.com>
2017-09-21 11:59:11 -0400

    update copy to include etc/host scripts

--- copy_files.sh
+++ copy_files.sh
@@ -10,6 +10,8 @@ cp ~/.vimrc .vimrc
 cp ~/.tmux.conf .tmux.conf
 cp ~/.gitignore_global .gitignore_global
 cp ~/.gitconfig .gitconfig
+cp ~/.time2work .time2work.sh
+cp ~/.time2play .time2play.sh
 mkdir ./.vim/
 cp -rf ~/.vim/colors/ ./.vim/colors/
 cp -rf ~/.tmuxinator/ ./.tmuxinator/
