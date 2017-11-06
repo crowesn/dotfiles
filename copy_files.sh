2a4f64ffa4dce67c3824220c39d68d151ce55b15
crowesn <crowesn@gmail.com>
2017-11-06 08:58:56 -0500

    update scripts

--- copy_files.sh
+++ copy_files.sh
@@ -10,8 +10,8 @@ cp ~/.vimrc .vimrc
 cp ~/.tmux.conf .tmux.conf
 cp ~/.gitignore_global .gitignore_global
 cp ~/.gitconfig .gitconfig
-cp ~/.time2work .time2work.sh
-cp ~/.time2play .time2play.sh
+cp ~/.time2work.sh .time2work.sh
+cp ~/.time2play.sh .time2play.sh
 mkdir ./.vim/
 cp -rf ~/.vim/colors/ ./.vim/colors/
 cp -rf ~/.tmuxinator/ ./.tmuxinator/
