d70798ef1c97f4152ef705306901ec2085bb204c
Sean Crowe <1069588+crowesn@users.noreply.github.com>
2024-12-18 09:39:49 -0500

    update dotfiles for m4

--- install_files.sh
+++ install_files.sh
@@ -2,19 +2,7 @@
 # zsh
 cp .zshrc ~/.zshrc
 cp .zprofile ~/.zprofile
-cp .zshenv ~/.zshenv
-cp .bash_osx ~/.bash_osx
-# bash
-cp .bashrc ~/.bashrc
-cp .profile ~/.profile
-cp .bash_profile ~/.bash_profile
-cp .bash_osx ~/.bash_osx
-cp .gitconfig ~/.gitconfig
-cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
-cp .gitignore_global ~/.gitignore_global
-cp .git-completion.bash ~/.git-completion.bash
 cp .gitconfig ~/.gitconfig
-mkdir ~/.vim/
-cp -rf ./.vim/colors/ ~/.vim/colors/
-cp -rf ./.tmuxinator/ ~/.tmuxinator/ 
+cp .gitignore_global ~/.gitignore_global
+cp .gitconfig ~/.gitconfig
